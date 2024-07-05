9f597e9f67d51b071b61b00f03a70a1cbee1463a
KeldeeFierce <keller.dm@yandex.ru>
2024-07-05 12:46:20 +0300

    script update

--- quick_start.sh
+++ quick_start.sh
@@ -1,17 +1,47 @@
 #!/bin/bash
 
 #needs npm to run CoC
-if which npm; then
-	echo "npm is installed"
-else
-	echo "Installing nvm"
-	if curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash; then
+# if which npm; then
+# 	echo "npm is installed"
+# else
+# 	echo "Installing nvm"
+# 	if curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash; then
+# 		echo "Success"
+# 	else
+# 		echo "Something whent wrong"
+# 		exit 1
+# 	fi
+# fi
+
+function check_install () {
+	if which "$1"; then
+		echo "$1 is installed"
+	else
+		echo "installing $1"
+		if $2; then
+			echo "Success"
+		else
+			echo "Something went wrong"
+			exit 1
+		fi 
+	fi
+}
+
+function install () {
+	echo "installing $1"
+	if $1; then
 		echo "Success"
 	else
-		echo "Something whent wrong"
+		echo "Something went wrong"
 		exit 1
 	fi
-fi
+}
+
+check_install "curl" "sudo apt install curl"
+check_install "npm" "curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash"
+check install "pip" "sudo apt install pip"
+install "pip install flake8 pylint bandit mypy pycodestyle black isort"
+check_install "shellcheck" "sudo apt install shellcheck"
 
 echo "PlugInstall"
 if vim -c "PlugInstall" +qa; then
@@ -21,13 +51,13 @@ else
 	exit 1
 fi
 
-echo "pip install"
-if pip install flake8 pylint bandit mypy pycodestyle blacki isort; then 
-	echo "Success"
-else
-	echo "Something went wrong"
-	exit 1
-fi
+# echo "pip install"
+# if pip install flake8 pylint bandit mypy pycodestyle blacki isort; then 
+# 	echo "Success"
+# else
+# 	echo "Something went wrong"
+# 	exit 1
+# fi
 
 echo "CocInstall python"
 if vim -c ":CocInstall coc-python" +qa; then
@@ -46,15 +76,13 @@ else
 fi
 
 
-echo "CocInstall sh"
-if sudo apt install shellcheck; then
-	echo "Success"
-else
-	echo "Something went wrong"
-	exit 1
-fi
-
-
+# echo "CocInstall sh"
+# if sudo apt install shellcheck; then
+# 	echo "Success"
+# else
+# 	echo "Something went wrong"
+# 	exit 1
+# fi
 
 
 
